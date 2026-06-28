c4d6fa50b93f7043758d915098c3246fbba3055d
RodrigoFFLima <rodrigofflima@hotmail.com>
2026-06-28 17:28:49 -0300

    Initial Commit

---
+++ AIBusinessAnalyst/AIBusinessAnalystApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIBusinessAnalystApp.swift
+//  AIBusinessAnalyst
+//
+//  Created by Rodrigo Ferrarezi Figueiredo de Lima on 28/06/26.
+//
+
+import SwiftUI
+
+@main
+struct AIBusinessAnalystApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
